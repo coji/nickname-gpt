0e7a35ffb68a00c8b8a0b7a2fd1617bd467fb1d0
coji <coji@techtalk.jp>
2023-03-07 12:42:03 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default MyApp
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,70 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
+import invariant from 'tiny-invariant'
 
-type Data = {
-  name: string
+invariant(process.env.OPENAI_API_KEY, 'OPENAI_API_KEY is not set')
+const conf = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(conf)
+
+const systemPrompt = `
+あなたは優秀なコピーライターです。これからユーザが入れるメールアドレスやIDの名前の付け方から、名前を決めた人がどんな人かを単語を元に類推してニックネームをつけてください。
+
+一つづつやりましょう。
+
+まずメールアドレスのパーツに含まれる単語を、日本語の言葉だと捉えて、それを元にどのような意味があるかを考えます。
+
+次にその意味に基づいて、その名前をつけた人がどのような趣味・嗜好があるかを推測します。
+
+最後に、趣味嗜好に合わせる形で、最初の日本語での意味を元に英語の単語を考え、それらをすべて組み合わせて英語圏で使えるニックネーム候補を生成してください。
+候補は５つ出してください。そのうち半分は、プロレスラーみたいなや歴戦の戦士みたいにめちゃくちゃかっこいいものにしてください。
+
+
+Output format:
+
+ニックネーム候補:
+  1. nickname1
+  2. nickname2
+  3. nickname3
+  4. nickname4
+  5. nickname5
+
+根拠:
+  ニックネームを考えた根拠
+
+ユーザがどのように感じるかの想像:
+  Assistantがどのように感じるかの想像
+`
+
+export type Data = {
+  request: string
+  result: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  invariant(req.method === 'POST', 'Only POST is allowed')
+  invariant(req.body, 'No request body')
+  invariant('request' in req.body, 'No "request" in request body')
+  const { request } = req.body
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: systemPrompt,
+      },
+      { role: 'user', content: request },
+    ],
+  })
+
+  res.status(200).json({
+    request,
+    result: response.data.choices[0].message?.content ?? 'No response',
+  })
 }
