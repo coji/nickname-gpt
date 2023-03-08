93dc76f4f0767b3d6b4574490624f6375b84edea
coji <coji@techtalk.jp>
2023-03-08 11:06:15 +0900

    緩和

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -60,7 +60,7 @@ const handler = async (req: Request): Promise<Response> => {
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
-      max_tokens: 512,
+      max_tokens: 800,
       stream: true,
       n: 1,
     }
