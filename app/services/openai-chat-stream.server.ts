95e9bdfb05196449641af7f466569b3399ccf62d
coji <coji@techtalk.jp>
2023-06-12 22:55:08 +0900

    feat: add openai api organization id

--- app/services/openai-chat-stream.server.ts
+++ app/services/openai-chat-stream.server.ts
@@ -48,6 +48,7 @@ export const OpenAIChatStream = async (
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ''}`,
+      'OpenAI-Organization': `${process.env.OPENAI_API_ORGANIZATION_ID ?? ''}`,
     },
     method: 'POST',
     body: JSON.stringify({
