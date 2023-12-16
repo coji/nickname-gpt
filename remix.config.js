b8d70bfdde0e04dd9238e4068b43d57e3c57bb2f
coji <coji@techtalk.jp>
2023-12-16 19:55:50 +0900

    Remove remix.config.js file

--- remix.config.js
+++
@@ -1,7 +0,0 @@
-import { flatRoutes } from 'remix-flat-routes'
-
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ['**/*'],
-  routes: async (defineRoutes) => flatRoutes('routes', defineRoutes),
-}
